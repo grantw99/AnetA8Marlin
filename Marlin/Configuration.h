21eff6b98425440c5b48f020a0fdfb2910f9d69b
shaneaw <51542213+shaneaw@users.noreply.github.com>
2020-12-26 10:34:44 -0500

    Adjust bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1122,8 +1122,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 210
+#define Y_BED_SIZE 195
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -40
